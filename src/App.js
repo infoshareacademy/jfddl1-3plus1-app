f576029b077360af061236be23a252d13c3c3267
Rafal <rafal.ciesielczuk@gmail.com>
2017-05-10 14:57:46 +0200

    ProductList v.1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductList from './ProductList'
 
 class App extends Component {
   render() {
@@ -13,9 +14,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ProductList/>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
---
+++ src/ProductList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductList = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
+
+export default ProductList
