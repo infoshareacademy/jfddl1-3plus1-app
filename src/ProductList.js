8d7fe30d46c89eea733aa1899067e0b3dd52d748
Rafal <rafal.ciesielczuk@gmail.com>
2017-05-10 15:37:59 +0200

    ProductList v.1.0

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
-const ProductList = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-)
+class ProductList extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: []
+    }
+
+    fetch(
+      process.env.PUBLIC_URL + '/data/products.json'
+    ).then(
+      response => response.json()
+    ).then(
+      products => this.setState({
+        products: products
+      })
+    )
+  }
+
+
+
+  render(){         //to sie zawsze powtarza
+    return(
+      <div>
+        {
+          this.state.products.map(
+            product => (
+              <ul key={product.id}>
+                <li>{product.name}</li>
+                <li>{product.studentIds}</li>
+                <li>{product.city}</li>
+                <li>{product.level}</li>
+
+              </ul>
+            )
+          )
+        }
+      </div>
+    )
+  }
+
+}
 export default ProductList
