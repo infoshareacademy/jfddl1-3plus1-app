cddd75c5602b1688f1c5c1770d93d3b0efa6a77c
Adrian Tarasiuk <adrian.tarasiuk23@gmail.com>
2017-05-11 15:41:31 +0200

    started FavouriteBrands component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Popup from './Popup'
+import FavouriteBrands from './FavouriteBrands'
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
       <div className="App">
           <h1>wyszukiwarka</h1>
           <h1>najpopularniejsze marki</h1>
-          <h1>twoje ulubione marki</h1>
+          <FavouriteBrands/>
           <Popup/>
       </div>
     );
