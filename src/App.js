e24ee2516343462860c6cbac72fb212453f59891
Rafal <rafal.ciesielczuk@gmail.com>
2017-05-11 15:48:21 +0200

    ProductList v.1.5

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <ProductList/>
-
           <h1>wyszukiwarka</h1>
           <h1>najpopularniejsze marki</h1>
           <FavouriteBrands/>
           <Popup/>
+
+        <ProductList/>
+
       </div>
     );
   }
