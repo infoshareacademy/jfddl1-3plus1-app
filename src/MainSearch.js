5d12a27da699e81e1051fc47e9c431f354de6f14
Adrian Tarasiuk <adrian.tarasiuk23@gmail.com>
2017-05-15 17:03:56 +0200

    added search from

---
+++ src/MainSearch.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Button, Grid, Row, Col } from 'react-bootstrap'
+
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+const getOptions = function(input, callback) {
+    setTimeout(function() {
+        callback(null, {
+            options: [
+                { value: 'one', label: 'One' },
+                { value: 'two', label: 'Two' }
+            ],
+            // CAREFUL! Only set this to true when there are no more options,
+            // or more specific queries will not be sent to the server.
+            complete: true
+        });
+    }, 500);
+};
+
+class MainSearch extends React.Component {
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <h4 className="text-center">Wyszukiwarka</h4>
+                    <Col xs={4} className="text-center">
+                            <h5>Model</h5>
+                        <Select.Async
+                            name="form-field-name"
+                            loadOptions={getOptions}
+                        />
+                    </Col>
+                    <Col xs={4} className="text-center">
+                                <h5>Marka</h5>
+                        <Select.Async
+                            name="form-field-name"
+                            loadOptions={getOptions}
+                        />
+                    </Col>
+                    <Col xs={4} className="text-center">
+                            <h5>Typ</h5>
+                        <Select.Async
+                            name="form-field-name"
+                            loadOptions={getOptions}
+                        />
+                    </Col>
+
+                    <Col xs={12} className="text-center">
+                            <Button>
+                                Szukaj
+                            </Button>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
+}
+
+export default MainSearch
