607c487bcd68eebc178700a42834e5562bfd377c
Adrian Tarasiuk <adrian.tarasiuk23@gmail.com>
2017-05-17 16:21:28 +0200

    Added logo to LoginPage

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Row, Col, Button, Jumbotron, FormGroup, FormControl } from 'react-bootstrap'
+import { Grid, Row, Col, Button, Jumbotron, FormGroup, FormControl, Image } from 'react-bootstrap'
 
 
 class LoginPage extends React.Component {
@@ -11,8 +11,8 @@ class LoginPage extends React.Component {
                     <Col md={4}>
                         <Button className="add-margin">Przejdź do aplikacji</Button>
                     </Col>
-                    <Col md={4} className="text-center">
-                        <h1>logo</h1>
+                    <Col md={4} className="logo-center">
+                        <Image responsive src={process.env.PUBLIC_URL + 'images/logo_projekt.png'} className="logo"/>
                     </Col>
                     <Col md={4} className="text-right">
                         <Button className="add-margin">Zaloguj się</Button>
