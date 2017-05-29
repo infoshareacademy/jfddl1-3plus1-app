a95424009b7eebdf5094c15ddf2bbceacb7b5701
Adrian Tarasiuk <Adrian@MacBook-Pro-AT.local>
2017-05-29 14:33:10 +0200

    Added login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,68 @@
 import React from 'react'
-import { FormGroup, FormControl, Button, Col } from 'react-bootstrap'
+import {FormGroup, FormControl, Button, Col} from 'react-bootstrap'
+import * as firebase from 'firebase'
+
 
 class LoginForm extends React.Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    loginHandler = (e) => {
+        e.preventDefault();
+        firebase.auth().auth.signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                console.log('Zalogowano');
+            }).catch((e => console.log(e.message)))
+    }
+
+    emailChangeHandler = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    passwordChangeHandler = (e) => {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
     render() {
+
+
+        // const txtEmail = document.getElementById('txtEmail');
+        // const txtPassword = document.getElementById('txtPassword');
+        // const btnLogin = document.getElementById('btnLogin');
+
+
+        // btnLogin.addEventListener('click',
+
+
         return (
-            <form>
+            <form onSubmit={this.loginHandler}>
                 <Col md={6} className="change-padding-right">
                     <FormGroup>
-                        <FormControl type="text" placeholder="Login"/>
+                        <FormControl required id="txtEmail" type="email" placeholder="Email"
+                                     onChange={this.emailChangeHandler} value={this.state.email}/>
                     </FormGroup>
                 </Col>
                 <Col md={6} className="change-padding-left">
                     <FormGroup>
-                        <FormControl type="email" placeholder="Hasło"/>
+                        <FormControl required id="txtPassword" type="password" placeholder="Pass"
+                                     onChange={this.passwordChangeHandler} value={this.state.password}/>
                     </FormGroup>
                 </Col>
                 <div className="text-center">
-                    <Button type="submit" className="text-center login-button-width">Zaloguj się</Button>
+                    <Button id="btnLogin" type="submit" className="text-center login-button-width">Zaloguj się</Button>
                 </div>
                 <div className="text-center">
                     <p className="p-margin">lub</p>
                 </div>
                 <div className="text-center">
-                    <Button type="submit" className="text-center login-button-width fb-button">Zaloguj się przez Facebook</Button>
+                    <Button type="submit" className="text-center login-button-width fb-button">Zaloguj się przez
+                        Facebook</Button>
                 </div>
             </form>
         )
