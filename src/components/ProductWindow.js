9f4a7537999341e815b5750c610b334583a6e75c
Rafal <rafal.ciesielczuk@gmail.com>
2017-05-31 14:23:54 +0200

    button 'kup Teraz'

--- src/components/ProductWindow.js
+++ src/components/ProductWindow.js
@@ -29,85 +29,97 @@ class ProductWindow extends React.Component {
   render() {
     return (
       <div align="center">
-        <h2>Informacje o produkcie</h2>
+        <h2 className="text-center">Informacje o produkcie</h2>
 
+        <Row>
+          <Col xs={12} md={6}>
+            <Tabs defaultActiveKey={1} id="tabMenu">
+              <Tab eventKey={1} title="Szczegółowe informacje ">
+                {
+                  this.state.stock === null ? <p>'Pobieranie danych…' </p> :
+                    <div>
+                      <Row>
+                        <Col xs={6} className="text-center">
+                          <Table striped bordered condensed hover>
+                            <thead>
 
-        <Tabs defaultActiveKey={1} id="tabMenu">
-          <Tab eventKey={1} title="Szczegółowe informacje ">
-            {
-              this.state.stock === null ? <p>'Pobieranie danych…' </p> :
-                <div>
-                  <Row>
-                    <Col xs={6} className="text-center">
-                      <Table striped bordered condensed hover>
-                        <thead>
+                            </thead>
 
-                        </thead>
+                            <tbody>
+                            <tr>
+                              <th>Nazwa</th>
+                              <td><p>{this.state.stock.part.data.brand}</p></td>
+                            </tr>
+                            <tr>
+                              <th>Model</th>
+                              <td><p>{this.state.stock.part.data.number}</p></td>
+                            </tr>
+                            <tr>
+                              <th>Photo</th>
+                              <img src={this.state.stock.part.jpg[0]} alt="No data"/>
+                            </tr>
+                            </tbody>
 
-                        <tbody>
-                        <tr>
-                          <th>Nazwa</th>
-                          <td><p>{this.state.stock.part.data.brand}</p></td>
-                        </tr>
-                        <tr>
-                          <th>Model</th>
-                          <td><p>{this.state.stock.part.data.number}</p></td>
-                        </tr>
-                        <tr>
-                          <th>Photo</th>
-                          <img src={this.state.stock.part.jpg[0]} alt="No data"/>
-                        </tr>
-                        </tbody>
+                          </Table>
+                        </Col>
+                      </Row>
+                    </div>
+                }
 
-                      </Table>
-                    </Col>
+              </Tab>
+              <Tab eventKey={2} title="Dostawa i płatność">
+                <ul class="c-tab-shipping-returns__list">
 
-                    <Col xs={12} md={4}>
-                      <tr>
-                        <h3>nazwa produktu</h3>
+                  <li>
+                    <h3 >Darmowa wysyłka od 150 zł!</h3>
+                    <p>UWAGA! Dla zamówień powyżej 150 zł nie musisz u nas płacić za wysyłkę!</p>
+                  </li>
 
-                        <Button><h4>SKLEP</h4> <h6>(opcjonalnie)</h6></Button>
-                      </tr>
-                    </Col>
-                  </Row>
-                </div>
-            }
+                  <li>
+                    <h3 class="o-heading__size4">Aż 30 dni na zwrot</h3>
+                    <p>Korzystając z oferowanego przez nas przedłużonego prawa do zwrotu, możesz zwrócić produkt nawet do
+                      30 dni po zakupie.</p>
+                  </li>
 
-          </Tab>
-          <Tab eventKey={2} title="Dostawa i płatność">
-            <ul class="c-tab-shipping-returns__list">
+                  <li>
+                    <h3 class="o-heading__size4">Bezpłatny zwrot</h3>
+                    <p>My płacimy za zwroty. W związku z tym konieczność odesłania produktu nie oznacza dla Ciebie
+                      dodatkowych kosztów.</p>
+                  </li>
+                </ul>
+              </Tab>
 
-              <li>
-                <h3 >Darmowa wysyłka od 150 zł!</h3>
-                <p>UWAGA! Dla zamówień powyżej 150 zł nie musisz u nas płacić za wysyłkę!</p>
-              </li>
+              <Tab eventKey={3} title="Skontaktuj sie z nami">
+                <ul class="c-tab-shipping-returns__list">
+                  <li>
+                    <h3 class="o-heading__size4">Porada konsultanta</h3>
+                    <p>Nie jesteś pewien czy któraś z części pasuje do Twojego auta? Nie przejmuj się! Dzwoń do nas. <p>Nasz
+                      konsultant czeka na Twój telefon. <strong>(45) 123-45-67 </strong></p></p>
+                  </li>
+                </ul>
+              </Tab>
 
-              <li>
-                <h3 class="o-heading__size4">Aż 30 dni na zwrot</h3>
-                <p>Korzystając z oferowanego przez nas przedłużonego prawa do zwrotu, możesz zwrócić produkt nawet do
-                  30 dni po zakupie.</p>
-              </li>
+            </Tabs>
+          </Col>
 
-              <li>
-                <h3 class="o-heading__size4">Bezpłatny zwrot</h3>
-                <p>My płacimy za zwroty. W związku z tym konieczność odesłania produktu nie oznacza dla Ciebie
-                  dodatkowych kosztów.</p>
-              </li>
-
-              <li>
-                <h3 class="o-heading__size4">Porada konsultanta</h3>
-                <p>Nie jesteś pewien czy któraś z części pasuje do Twojego auta? Nie przejmuj się! Dzwoń do nas. Nasz
-                  konsultant czeka na Twój telefon. <strong>(45) 123-45-67 </strong></p>
-              </li>
-
-            </ul>
-          </Tab>
-        </Tabs>
+          <Col xs={12} md={6}>
+            <tr>
+              <td>
+                  <a href="https://allegro.pl/">
+                    <Button>
+                      <h4>KUP TERAZ</h4>
+                    </Button>
+                  </a>
+              </td>
+              <td>
+                <h5>Już teraz możesz zamówić wybrany produkt</h5>
+              </td>
+            </tr>
+          </Col>
+        </Row>
       </div>
-
     )
   }
-
 }
 
-export  default ProductWindow
+export default ProductWindow
