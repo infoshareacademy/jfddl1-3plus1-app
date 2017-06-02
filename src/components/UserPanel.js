585908950773a6da6ebb9dbec33f5de02aba65a0
Rafal <rafal.ciesielczuk@gmail.com>
2017-06-02 16:25:01 +0200

    making profile account

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -19,34 +19,38 @@ const Saved = () => (
         <h2>Zapisane</h2>
     </div>
 );
-
 class UserPanel extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Grid>
-                    <Row>
-                        <Col md={4}>
+  render() {
+    return (
+      <Router>
+          <Grid>
+              <Row>
+                  <Col md={12}>
+                      <h1>Profil użytkownika</h1>
+                  </Col>
+                  <Col md={4}>
+                      <Image className="image-size" circle responsive src={process.env.PUBLIC_URL + '/images/face.jpg'}/>
+                      <ul>
+                          <li><Link to="/userPanel/profile">Profil</Link></li>
+                          <li><Link to="/userPanel/settings">Ustawienia</Link></li>
+                          <li><Link to="/userPanel/saved">Zapisane</Link></li>
+                      </ul>
+                  </Col>
 
-                            <Image className="image-size" circle responsive src={process.env.PUBLIC_URL + '/images/search_placeholder.png'}/>
+                  <Col md={8}>
+                      <h2>Login:</h2>
+                      <h2>imię:</h2>
+                      <h2>info</h2>
 
-                            <ul>
-                                <li><Link to="/userPanel/profile">Profil</Link></li>
-                                <li><Link to="/userPanel/settings">Ustawienia</Link></li>
-                                <li><Link to="/userPanel/saved">Zapisane</Link></li>
-                            </ul>
-                        </Col>
-
-                        <Col md={8}>
-                            <Route exact path="/userPanel/profile" component={Profile}/>
-                            <Route path="/userPanel/settings" component={Settings}/>
-                            <Route path="/userPanel/saved" component={Saved}/>
-                        </Col>
-                    </Row>
-                </Grid>
-            </Router>
-        )
-    }
+                      <Route exact path="/userPanel/profile" component={Profile}/>
+                      <Route path="/userPanel/settings" component={Settings}/>
+                      <Route path="/userPanel/saved" component={Saved}/>
+                  </Col>
+              </Row>
+          </Grid>
+      </Router>
+    )
+  }
 }
 
 export default UserPanel
