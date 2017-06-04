7189a45060c350466047ff300f03cbedd1ded9e7
Adrian Tarasiuk <Adrian@MacBook-Pro-AT.local>
2017-06-04 15:40:54 +0200

    Footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,36 @@
 import React from 'react'
-import { Grid, Row, Col, Glyphicon } from 'react-bootstrap'
+import { Grid, Row, Col, Glyphicon, Image } from 'react-bootstrap'
 
-const Style ={
+const backgroundStyle ={
     backgroundImage: `url('${process.env.PUBLIC_URL}/images/footer_lodyas.png')`
 }
 
 class Footer extends React.Component {
     render () {
         return (
-            <Grid className="text-center container-fluid footer" style={Style}>
+            <Grid className="text-center container-fluid footer" style={backgroundStyle}>
                 <Row>
                     <Col xs={12}>
                         <div>
-                            <p>logo</p>
-                            <a href="">Polityka prywatności</a>
-                            <a href="">Polityka Cookies </a>
-                            <a href="">Regulamin</a>
+                            <div className="footer-logo">
+                                <Image responsive
+                                       src={process.env.PUBLIC_URL + '/images/logo_projekt_smaller.png'}/>
+                            </div>
+                            <a className="change-link-color" href="">Polityka prywatności</a>
+                            <span><Glyphicon className="glyphicon-one-dot" glyph=""></Glyphicon></span>
+                            <a className="change-link-color" href="">Polityka Cookies </a>
+                            <span><Glyphicon className="glyphicon-one-dot" glyph=""></Glyphicon></span>
+                            <a className="change-link-color" href="">Regulamin</a>
                         </div>
-                        <div>
+                        <div id="changeLinkMargin">
+                            <a target="_blank" href="https://pl-pl.facebook.com/"><img src="images/facebook_icon.png"/></a>
+                            <a target="_blank" href="https://twitter.com/?lang=pl"><img src="images/twitter_icon.png"/></a>
+                            <a target="_blank" href="https://plus.google.com/collections/featured"><img src="images/googleplus_icon.png"/></a>
+                            <a target="_blank" href="https://www.youtube.com/?hl=pl&gl=PL"><img src="images/youtube_icon.png"/></a>
                         </div>
                     </Col>
                 </Row>
-                <Row>
+                <Row id="remove-margin-p">
                     <p>Stworzone z <Glyphicon className="glyphicon-heart" glyph=""></Glyphicon> przez 3plus1.</p>
                     <p><Glyphicon className="glyphicon-copyright-mark" glyph=""></Glyphicon>2017 Wszelkie prawa zastrzeżone.</p>
                 </Row>
